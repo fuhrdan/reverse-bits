fd67bfd4f74b25ae540753377d811cb83bfaf1a3
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2026-02-16 06:30:14 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//*****************************************************************************
+//** 190. Reverse Bits                                              leetcode **
+//*****************************************************************************
+//** Take the bits and reverse them through
+//** an inverse answer will show true
+//** With a binary shift 
+//** The answer is a gift
+//*****************************************************************************
+
+int reverseBits(int n) {
+    int ans = 0;
+    for(int i = 0; i < 32 && n; i++)
+    {
+        ans |= (n & 1) << (31 - i);
+        n >>= 1;
+    }
+    return ans;
+}
